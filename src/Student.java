98e2989b8e415799db2c3cf56512ffe654323628
niravgadhia <niravgadhia@gmail.com>
2016-10-08 11:51:54 -0400

    added java files

---
+++ src/Student.java
@@ -0,0 +1,11 @@
+import java.util.Comparator;
+
+public class Student {
+	private String name;
+	private int points;
+	
+	public Student(){
+		
+	}
+	
+}
---
+++ src/StudentTester.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * This is the entry point for Team Assignment 1
+ * @author Team Demo
+ *
+ */
+public class StudentTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
