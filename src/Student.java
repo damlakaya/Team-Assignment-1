0cc59c0b199fd8da4b02fdf17e4f14057f3e5ee3
damlakaya <dkaya94@gmail.com>
2016-10-21 13:14:51 -0400

    Update Student.java

--- src/Student.java
+++ src/Student.java
@@ -6,6 +6,49 @@ public class Student {
 	
 	public Student(){
 		
-	}
+import java.util.Comparator; 
+
+
+public class student{
+
+	private String name; 
+	private int points; 
 	
+	
+	public student(String string, int i) {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+
+	public String toMyString() {
+		// TODO Auto-generated method stub
+		return name + ":" + points + ":" + getGrades();
+	}
+
+
+	private char getGrades() {
+		// TODO Auto-generated method stub
+		return 'A';
+	}
+
+	public static Comparator<? super student> compareTo() {    // ranks the grades of students. 
+		// TODO Auto-generated method stub
+		return null;
+	
+	}
 }
+
+
+
+
+
+
+
+
+
+
